a6db6770e3c2d24e8184b4a012f1b95d12ab19e6
Gabriel Augusto <gafb.xpenergy@gmail.com>
2023-05-28 17:55:59 -0300

    section 01-fundamentos: Sidebar and use of css

---
+++ 01-fundamentos/src/App.jsx
@@ -0,0 +1,27 @@
+import Post from './Post';
+import { Header } from './components/Header';
+
+import './global.css'
+import styles from './App.module.css'
+import { Sidebar } from './components/Sidebar';
+
+function App() {
+
+  return (
+    <div>
+      <Header/>
+      
+
+      <div className={styles.wrapper}>
+        <Sidebar/>
+
+        <main>
+        <Post/>
+        </main>
+      
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ 01-fundamentos/src/Post.jsx
@@ -0,0 +1,4 @@
+function Post() {
+    return <p>Post</p>
+}
+export default Post;
