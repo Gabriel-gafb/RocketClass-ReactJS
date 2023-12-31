7f248803e60ad58adee3780e813ae648c642f3b4
Gabriel Augusto <gafb.xpenergy@gmail.com>
2023-12-31 17:10:39 -0300

    03: api connection pt1

---
+++ dt-money-03/src/contexts/TransactionsContext.tsx
@@ -0,0 +1,38 @@
+import { ReactNode, createContext, useEffect, useState } from "react";
+
+interface Transaction {
+  id: number;
+  description: string;
+  type: "income" | "outcome";
+  price: number;
+  category: string;
+  createdAt: string;
+}
+
+interface TransactionContentType {
+  transactions: Transaction[];
+}
+
+interface TransactionProviderProps {
+  children: ReactNode;
+}
+
+export const TransactionsContext = createContext({} as TransactionContentType);
+
+export function TransactionsProvider({ children }: TransactionProviderProps) {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+  async function loadTransactions() {
+    const response = await fetch("http://localhost:3000/transactions");
+    const data = await response.json();
+    setTransactions(data);
+  }
+  useEffect(() => {
+    loadTransactions();
+  }, []);
+  return (
+    <TransactionsContext.Provider value={{ transactions }}>
+      {children}
+    </TransactionsContext.Provider>
+  );
+}
--- dt-money-03/src/pages/Transactions/index.tsx
+++ dt-money-03/src/pages/Transactions/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useContext } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { SearchForm } from "./components/SearchForm";
@@ -7,26 +7,11 @@ import {
   TransactionContainer,
   TransactionsTable,
 } from "./styles";
+import { TransactionsContext } from "../../contexts/TransactionsContext";
+import { dateFormatter, priceFormatter } from "../../utils/formatter";
 
-interface Transaction {
-  id: number;
-  description: string;
-  type: "income" | "outcome";
-  price: number;
-  category: string;
-  createdAt: string;
-}
 export function Transactions() {
-  const [transactions, setTransactions] = useState<Transaction[]>([]);
-
-  async function loadTransactions() {
-    const response = await fetch("http://localhost:3000/transactions");
-    const data = await response.json();
-    setTransactions(data);
-  }
-  useEffect(() => {
-    loadTransactions();
-  }, []);
+  const { transactions } = useContext(TransactionsContext);
   return (
     <div>
       <Header />
@@ -42,11 +27,14 @@ export function Transactions() {
                   <td width="50%">{transaction.description}</td>
                   <td>
                     <PriceHighlight variant={transaction.type}>
-                      {transaction.price}
+                      {transaction.type === "outcome" && "- "}
+                      {priceFormatter.format(transaction.price)}
                     </PriceHighlight>
                   </td>
                   <td>{transaction.category}</td>
-                  <td>{transaction.createdAt}</td>
+                  <td>
+                    {dateFormatter.format(new Date(transaction.createdAt))}
+                  </td>
                 </tr>
               );
             })}
